3fa991d3134675f275babaf718570159e9af5ed5
Teodoro Barba <teodoro.barba4022@alumnos.udg.mx>
2020-10-18 21:25:51 -0500

    Implementacion de los metodos de computadora

--- computadora.cpp
+++ computadora.cpp
@@ -8,11 +8,13 @@ Computadora::Computadora()
 
 Computadora::Computadora(const string &sisOperativo, 
                 const string &NomEquipo,
+                const string &Marca,
                 int Ram,
                 int Cant_nucleos)
 {
     this->sisOperativo = sisOperativo;
     this->NomEquipo = NomEquipo;
+    this->Marca = Marca;
     this->Ram = Ram;
     this->Cant_nucleos = Cant_nucleos;
 }
@@ -31,10 +33,22 @@ void Computadora::setNombre(const string &v)
 {
     NomEquipo=v;
 }
+
 string Computadora::getNombre()
 {
     return NomEquipo;
 }
+
+void Computadora::setMarca(const string &v)
+{
+    Marca=v;
+}
+
+string Computadora::getMarca()
+{
+    return Marca;
+}
+
 void Computadora::setRam(int v)
 {
     Ram=v;
