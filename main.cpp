6dbbba9301e6bf02346ca18ec0a9028ca30f8e3c
Teodoro Barba <teodoro.barba4022@alumnos.udg.mx>
2020-10-18 21:33:38 -0500

    Se creo una computadora, se capturo la informacion usando el operador sobrecargado >>, se agrego la computadora a la clase laboratori con el operador << y se mostro la informacion en forma de tabla.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
-#include "oficina.h"
+#include "Laboratorio.h"
 
 using namespace std;
 
 int main(){
     Computadora pc1 = Computadora( "Windows 10",
                                     "Pc_casa",
+                                    "Lenovo",
                                     8.0,
                                     4);
     
 
-    Computadora pc3;
-    pc3.setOperativo("Ubuntu");
-    pc3.setNombre("Oficina");
-    pc3.setRam(8);
-    pc3.setNucleos(4);
+    Computadora pc2;
+    pc2.setOperativo("Ubuntu");
+    pc2.setNombre("Oficina");
+    pc2.setMarca("Asus");
+    pc2.setRam(8);
+    pc2.setNucleos(4);
 
-    Oficina of;
+    Laboratorio lab;
+    Computadora pc3, pc4;
+    
+   
+    cin >> pc4;
+    //cout<< pc3;
+    lab << pc1 << pc2 << pc4;
+    lab.mostrar();
+    //of.agregarComputadora(pc1);
+    //of.agregarComputadora(pc3);
+    
     
 
-    of.agregarComputadora(pc1);
-    of.agregarComputadora(pc3);
+    //cout<< pc3;
+    //Computadora pc4;
+    //cin >> pc4;
+    //lab << pc4;
 
-    of.mostrar();
+    
 /*
     cout << pc3.getOperativo() <<endl;
     cout << pc3.getNombre() <<endl;
@@ -30,3 +44,4 @@ int main(){
     cout << pc3.getNucleos() <<endl;*/
     return 0;
     }
+
