be9b7bf741d12fc668349c0a4d35b97407a6c64d
Teodoro Barba <teodoro.barba4022@alumnos.udg.mx>
2020-10-18 21:27:53 -0500

    Se añadio la sobrecarga del operador << que permite agregar objetos computadora a la clase laboratorio

---
+++ laboratorio.h
@@ -0,0 +1,23 @@
+#ifndef LABORATORIO_H
+#define LABORATORIO_H
+
+#include "computadora.h"
+
+class Laboratorio
+{
+    Computadora arreglo[5];
+    size_t cont;
+public:
+    Laboratorio();
+    void agregarComputadora(const Computadora &p);
+    void mostrar();
+
+    friend Laboratorio& operator<<(Laboratorio &l, const Computadora &p)
+    {
+        l.agregarComputadora(p);
+
+        return l;
+    }
+};
+
+#endif
